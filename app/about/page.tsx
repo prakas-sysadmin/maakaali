471430463a4e404da58780a42dd11bf315d48857
PrakasRana_DevOps <80772262+prakas-sysadmin@users.noreply.github.com>
2026-05-17 09:51:09 +0530

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,14 +6,14 @@ export default function About() {
         <p className="text-lg mb-6">
           Welcome to the official digital home of the Sahaspur Siddeswari Kali Mandir, a sanctuary of faith, healing, and divine grace located in the heart of Paschim Medinipur, West Bengal.
         </p>
-        <p className="text-lg mb-6">
+        <p className="text-lg mb-6 ">
           For generations, our temple has stood as a beacon of hope for countless devotees. Here, Maa Kali is not just worshipped as a deity, but revered as a compassionate mother who listens to the deepest cries of her children.
         </p>
         <h2 className="text-2xl font-bold text-center mb-8 text-gold-500">Our Legacy of Divine Healing ></h2>
-        <p className="text-lg">
+        <p className="text-lg mb-6">
           The Sahaspur Siddeswari Kali Mandir holds a unique and profound place in the hearts of people across the region. It is widely believed and experienced that through the boundless blessings (asirbad) of Maa Kali, many individuals suffering from severe mental illnesses and distress have found complete healing and peace of mind.
         </p>
-        <p className="text-lg">
+        <p className="text-lg mb-6">
           Where modern medicine and worldly efforts sometimes falter, the divine intervention of Maa Siddeswari begins. Countless families have witnessed the miraculous recovery of their loved ones (manosik pagol manus), who have returned to their normal, healthy lives by the grace of the Mother.
         </p>
       </div>
