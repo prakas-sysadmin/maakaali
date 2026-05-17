ac7cb52fcacbc413da930200e871585eeb1b0676
PrakasRana_DevOps <80772262+prakas-sysadmin@users.noreply.github.com>
2026-05-17 09:52:35 +0530

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -9,7 +9,7 @@ export default function About() {
         <p className="text-lg mb-6 ">
           For generations, our temple has stood as a beacon of hope for countless devotees. Here, Maa Kali is not just worshipped as a deity, but revered as a compassionate mother who listens to the deepest cries of her children.
         </p>
-        <h2 className="text-2xl font-bold text-center mb-8 text-gold-500">Our Legacy of Divine Healing ></h2>
+        <h2 className="text-2xl font-bold text-center mb-8 text-gold-500">Our Legacy of Divine Healing </h2>
         <p className="text-lg mb-6">
           The Sahaspur Siddeswari Kali Mandir holds a unique and profound place in the hearts of people across the region. It is widely believed and experienced that through the boundless blessings (asirbad) of Maa Kali, many individuals suffering from severe mental illnesses and distress have found complete healing and peace of mind.
         </p>
